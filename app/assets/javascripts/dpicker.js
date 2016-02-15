72f7441d39631c76249b7ae2d483adfc269328fd
natasha <pospelovanm@gmail.com>
2016-02-15 10:40:28 +0200

    changes to datepicker

--- app/assets/javascripts/dpicker.js
+++ app/assets/javascripts/dpicker.js
@@ -2,6 +2,7 @@ $(function() {
     $( "#dp" ).datepicker({
         changeMonth: true,
         changeYear: true,
-        yearRange: '1950:2016'
+        yearRange: '1950:2016',
+        dateFormat: 'dd-mm-yy'
     });
 });
--- test/controllers/pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PagesControllerTest < ActionController::TestCase
-  test "should get activity" do
-    get :activity
-    assert_response :success
-  end
-
-end
